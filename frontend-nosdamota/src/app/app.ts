b4844d07bf796a0c0c640d3e9d4a32b233cc6c2d
Arthur <arthur07hernandes@hotmail.com>
2026-02-25 10:31:40 -0300

    posicionando o header que tinha removido

--- frontend-nosdamota/src/app/app.ts
+++ frontend-nosdamota/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
