8d46ec3cef0690eba6584c2fcca21bb30109dec4
Arthur <arthur07hernandes@hotmail.com>
2026-03-10 10:25:10 -0300

    arrumei a tela de login estava sem estilização

--- projetonosdamota/src/main/java/com/ruthra/projetonosdamota/controller/OrderController.java
+++ projetonosdamota/src/main/java/com/ruthra/projetonosdamota/controller/OrderController.java
@@ -5,13 +5,11 @@ import com.ruthra.projetonosdamota.model.Order;
 import com.ruthra.projetonosdamota.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
