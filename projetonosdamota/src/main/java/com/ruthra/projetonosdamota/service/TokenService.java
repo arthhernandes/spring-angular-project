2e02b2f60c8b655fccba8651400532a53c6240b3
Arthur <arthur07hernandes@hotmail.com>
2026-03-11 09:55:30 -0300

    implementação do jwt

---
+++ projetonosdamota/src/main/java/com/ruthra/projetonosdamota/service/TokenService.java
@@ -0,0 +1,51 @@
+package com.ruthra.projetonosdamota.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToker(User user) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+            return Jwts.builder()
+                    .subject(user.getUsername())
+                    .issuedAt(new Date())
+                    .expiration(new Date(System.currentTimeMillis() + 86400000))
+                    .signWith(key)
+                    .compact();
+        }catch (Exception e){
+            throw new RuntimeException("Erro ao gerar token JWT", e);
+        }
+    }
+
+    public String validateToker(String token) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+            return Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload()
+                    .getSubject();
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+
+}
