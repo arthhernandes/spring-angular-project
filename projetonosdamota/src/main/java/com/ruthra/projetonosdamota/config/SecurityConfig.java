8b5b7da1e279ac53c486a1fc01187a3751adb795
Arthur <arthur07hernandes@hotmail.com>
2026-04-30 12:08:48 -0300

    a

--- projetonosdamota/src/main/java/com/ruthra/projetonosdamota/config/SecurityConfig.java
+++ projetonosdamota/src/main/java/com/ruthra/projetonosdamota/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
                         .requestMatchers("/api/customer/**").authenticated()
